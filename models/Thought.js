fe16fa70acfc089ee5a2d3cf37edd4677fc35a36
McHenry <lukemchenry311@gmail.com>
2021-10-14 23:22:25 -0600

    reorder thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,33 @@
 const { Schema, model, Types } = require("mongoose");
 
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: "Reaction Required",
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: "Username Required",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdatVal),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
@@ -26,34 +54,6 @@ const ThoughtSchema = new Schema(
     },
 
     id: false,
-  }
-);
-
-const ReactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new Types.ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: "Reaction Required",
-      maxlength: 280,
-    },
-    username: {
-      type: String,
-      required: "Username Required",
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: (createdAtVal) => dateFormat(createdatVal),
-    },
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
   }
 );
 
