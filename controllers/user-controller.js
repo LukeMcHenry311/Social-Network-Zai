fd34b66fb18bc7cf2dafdc0e2250697d600046c2
McHenry <lukemchenry311@gmail.com>
2021-10-15 05:16:05 -0600

    final code change

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,21 +3,21 @@ const { User } = require("../models");
 const UserController = {
   getAllUsers(req, res) {
     User.find({})
-    .populate({
-      path: 'thoughts',
-      select: '-__V'
-    })
-    .populate({
-      path: 'friends',
-      select: '-__V'
-    })
-    .select('-__V')
-    .sort({ _id: -1 })
-    .then(dbUserData => res.json(dbUserData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    })
+      .populate({
+        path: "thoughts",
+        select: "-__V",
+      })
+      .populate({
+        path: "friends",
+        select: "-__V",
+      })
+      .select("-__V")
+      .sort({ _id: -1 })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
   },
 
   getUserById({ params }, res) {
@@ -61,7 +61,7 @@ const UserController = {
     })
       .then((dbUser) => {
         if (!dbUser) {
-          res.status(404).json({ message: "no user found" });
+          res.status(404).json({ message: "No User found with this Id" });
           return;
         }
         res.json(dbUser);
