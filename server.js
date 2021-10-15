85766a5a79755dab6c952efa9a1fd15cd1802145
McHenry <lukemchenry311@gmail.com>
2021-10-14 22:58:59 -0600

    server connected

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-network-zai",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`connected on localhost${PORT}`));
